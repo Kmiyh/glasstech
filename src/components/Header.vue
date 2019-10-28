b7dfe55be3991a91ac82610e3be95b4f672e92fd
Pavel Galiguzov <pvlgaliguzov@gmail.com>
2019-10-28 18:07:21 +0300

    add

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    id="header"
-    class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom shadow-sm"
-  >
+  <div id="header" class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom shadow-sm">
     <h5 id="name" class="my-0 mr-md-auto font-weight-normal">{{sitename}}</h5>
     <nav id="menu" class="my-2 my-md-0 mr-md-3">
       <a class="p-2" href="#">Главная</a>
@@ -27,6 +24,8 @@ export default {
 <style scoped>
 #header {
   background-color: #3b5998;
+  margin-bottom: 0px !important;
+  border-bottom: 0px !important;
 }
 #name {
   font-size: 36px;
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,10 +2,18 @@
   <div>
     <my-header></my-header>
     <main>
-      <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-        <h1 class="display-4">{{head}}</h1>
-        <p class="lead">{{description}}</p>
-      </div>
+      <section>
+        <div id="image"></div>
+      </section>
+      <section>
+        <div class="container">
+          <h1 v-html="first"></h1>
+          <div class="row centered">
+            <div id="info">{{ff}}</div>
+            <button id="order" class="btn btn-warning">{{button}}</button>
+          </div>
+        </div>
+      </section>
     </main>
   </div>
 </template>
@@ -16,11 +24,107 @@ export default {
   name: "imain",
   data() {
     return {
-      head: "Pricing",
-      description:
-        "Quickly build an effective pricing table for your potential customers with this Bootstrap example. It’s built with default Bootstrap components and utilities with little customization."
+      button: "СДЕЛАТЬ ЗАКАЗ",
+      first: "ЗАЩИТНЫЕ СТЕКЛА ДЛЯ<br>МОБИЛЬНЫХ ТЕЛЕФОНОВ",
+      ff:
+        'Наша компания занимается оптовой продажей защитных стекол для смартфонов по всей России. У нас Вы можете выбрать защитное стекло для многих марок смартфонов. Мы гарантируем высокое качество защитных стекол и самые низкие цены на них. Оформите заказ в несколько шагов, нажав "сделать заказ"',
+      second: "Наши преимущества",
+      sf: "Собственное производство",
+      ss: "мониторинг рынка",
+      st: "Самые низкие цены",
+      sff:
+        "­­Реализуемые нами защитные стекла производятся на собственной фабрике! Мы не сотрудничаем с поставщиками стекол, потому что можем гарантировать качество только выпущенной нами продукции. Доверие клиентов – то, чем мы очень дорожим.",
+      ssf:
+        "Мы в курсе всех тенденций на рынке мобильных устройств! Смартфонам с самыми хрупкими экранами – самые надежные защитные стекла.",
+      stf:
+        "Мы гарантируем не только высокое качество продукции, но и самые низкие цены на защитные стекла. Цена товара Вас приятно удивит!­­ ",
+      third: "Виды защитных стекол",
+      fourth: "Понравилось?"
     };
   },
   components: { MyHeader }
 };
 </script>
+
+<style scoped>
+#image {
+  background-image: url("../../static/image/main.jpg");
+  background-position: center;
+  min-height: 680px;
+  width: 100%;
+}
+.container {
+  text-align: center;
+  padding-top: 20px;
+}
+#info {
+  text-align: left;
+  max-width: 700px;
+}
+#order {
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
+.btn-warning {
+  background: #e5ae09;
+  background: -moz-linear-gradient(
+    -45deg,
+    #e5ae09 0%,
+    #ffd044 50%,
+    #ffc107 51%,
+    #fc9014 71%,
+    #f1890b 100%
+  );
+  background: -webkit-linear-gradient(
+    -45deg,
+    #e5ae09 0%,
+    #ffd044 50%,
+    #ffc107 51%,
+    #fc9014 71%,
+    #f1890b 100%
+  );
+  background: linear-gradient(
+    135deg,
+    #e5ae09 0%,
+    #ffd044 50%,
+    #ffc107 51%,
+    #fc9014 71%,
+    #f1890b 100%
+  );
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#e5ae09', endColorstr='#f1890b',GradientType=1 );
+
+  background-size: 400% 400%;
+  -webkit-animation: AnimationName 3s ease infinite;
+  -moz-animation: AnimationName 3s ease infinite;
+  animation: AnimationName 3s ease infinite;
+  -webkit-animation: AnimationName 3s ease infinite;
+  -moz-animation: AnimationName 3s ease infinite;
+  animation: AnimationName 3s ease infinite;
+  border: medium none;
+  color: #fff !important;
+}
+@keyframes AnimationName {
+  0% {
+    background-position: 0% 31%;
+  }
+  50% {
+    background-position: 100% 70%;
+  }
+  100% {
+    background-position: 0% 31%;
+  }
+}
+.btn-outline-light:hover {
+  color: #3f345f;
+}
+.btn-outline-warning:hover {
+  color: #ffffff;
+}
+.btn-sm {
+  font-size: 12px;
+  padding: 11px 25px;
+}
+.elements-page-btn .btn {
+  margin: 6px 3px;
+}
+</style>
