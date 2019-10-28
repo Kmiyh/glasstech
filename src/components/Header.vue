ec11ad196f6674288be8daec7ad591c550ebf946
Pavel Galiguzov <pgaliguz@gmail.com>
2019-10-28 14:59:21 +0300

    add

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-    <h5 class="my-0 mr-md-auto font-weight-normal">{{sitename}}</h5>
-    <nav class="my-2 my-md-0 mr-md-3">
-      <a class="p-2 text-dark" href="#">Features</a>
-      <a class="p-2 text-dark" href="#">Enterprise</a>
-      <a class="p-2 text-dark" href="#">Support</a>
-      <a class="p-2 text-dark" href="#">Pricing</a>
+  <div
+    id="header"
+    class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom shadow-sm"
+  >
+    <h5 id="name" class="my-0 mr-md-auto font-weight-normal">{{sitename}}</h5>
+    <nav id="menu" class="my-2 my-md-0 mr-md-3">
+      <a class="p-2" href="#">Главная</a>
+      <a class="p-2" href="#">Продукты</a>
+      <a class="p-2" href="#">Новости</a>
+      <a class="p-2" href="#">Контакты</a>
     </nav>
-    <a class="btn btn-outline-primary" href="#">Sign up</a>
   </div>
 </template>
 
@@ -22,4 +24,19 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#header {
+  background-color: #3b5998;
+}
+#name {
+  font-size: 36px;
+  font: bold;
+  color: white;
+}
+#menu {
+  font-size: 20px;
+}
+#menu > a {
+  color: white;
+}
+</style>
