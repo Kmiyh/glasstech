a9416f435ec2f9e182bae8dde468ec7f1d715a53
Pavel Galiguzov <pgaliguz@gmail.com>
2019-10-31 11:29:27 +0300

    add

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -6,11 +6,16 @@
         <div class="container">
           <div class="card">
             <div class="row">
-              <div class="col-md-5 col-md-offset-0">
+              <div>
                 <figure>
                   <img :src="product.image" />
                 </figure>
               </div>
+              <div class="title">
+                <h3>{{product.title}}</h3>
+                <p>{{product.description}}</p>
+              </div>
+              <div></div>
             </div>
           </div>
         </div>
@@ -41,10 +46,18 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:300|Poiret+One|Roboto&display=swap");
+.title {
+  padding-top: 10px;
+  padding-right: 30px;
+  padding-left: 30px;
+  font-family: "Roboto", sans-serif;
+}
 .card {
   margin-top: 10px;
   margin-bottom: 10px;
   box-shadow: 0 0 7px rgba(0, 0, 0, 0.5);
+  max-width: 500px;
 }
 figure {
   margin: 5px 5px 5px 5px;
