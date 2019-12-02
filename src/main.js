2390599453979867b3135e181807979476e6c412
Pavel Galiguzov <pvlgaliguzov@gmail.com>
2019-12-02 14:51:05 +0300

    add

--- src/main.js
+++ src/main.js
@@ -17,14 +17,13 @@ Vue.use(VueScrollReveal);
 Vue.use(VueTelInput)
 Vue.use(firestorePlugin);
 firebase.initializeApp({
-  apiKey: "AIzaSyBCujIkPsUMXrywaiAgU9JEzAj4XtaqFZo",
-  authDomain: "glasstech-34465.firebaseapp.com",
-  databaseURL: "https://glasstech-34465.firebaseio.com",
-  projectId: "glasstech-34465",
-  storageBucket: "glasstech-34465.appspot.com",
-  messagingSenderId: "415886050787",
-  appId: "1:415886050787:web:7196c393c488bb810b8cd6",
-  measurementId: "G-XJ63M6SKK5"
+  apiKey: "AIzaSyAvlZ21px091cKs-WElvDIZOfRRB824fd8",
+  authDomain: "glass-6d5f0.firebaseapp.com",
+  databaseURL: "https://glass-6d5f0.firebaseio.com",
+  projectId: "glass-6d5f0",
+  storageBucket: "glass-6d5f0.appspot.com",
+  messagingSenderId: "46067386764",
+  appId: "1:46067386764:web:c7bbcaeecd0b123f982ea9"
 })
 export const db = firebase.firestore()
 Vue.config.productionTip = false;
