4110b8ed88c866ba993a3516995d065a36b5452c
Pavel Galiguzov <pgaliguz@gmail.com>
2019-12-03 10:50:29 +0300

    add

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,29 @@
-const functions = require('firebase-functions');
-const admin = require("firebase-admin")
-const nodemailer = require('nodemailer');
-admin.initializeApp()
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+admin.initializeApp();
 //google account credentials used to send email
 var transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
-    user: 'pasha.tatarin57@gmail.com',
-    pass: '235687_Rossia'
+    user: "pasha.tatarin57@gmail.com",
+    pass: "235687_Rossia"
   }
 });
 exports.sendEmail = functions.firestore
-  .document('orders/{orderId}')
+  .document("orders/{orderId}")
   .onCreate((snap, context) => {
-    console.log('success')
+    console.log("success");
     const mailOptions = {
-      from: 'nefritenot@gmail.com',
+      from: "nefritenot@gmail.com",
       to: snap.data().email,
-      subject: 'contact form message',
+      subject: "contact form message",
       html: `<h1>Order Confirmation</h1>
                                 <p>
                                    <b>Email: </b>${snap.data().email}<br>
                                 </p>`
     };
-    return transporter.sendMail(mailOptions, (error, data) => {
-      if (error) {
-        console.log(error)
-        return
-      }
-      console.log("Sent!")
-    });
+    return transporter.sendMail(mailOptions);
   });
